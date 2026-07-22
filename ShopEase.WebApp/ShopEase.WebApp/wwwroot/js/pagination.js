abe8d336bd85ea0d2d7e551f8290df4b23fb6d77
imbhabani00 <i.m.bhabani.00@gmail.com>
2026-07-23 00:08:34 +0530

    Roles module completed

--- ShopEase.WebApp/ShopEase.WebApp/wwwroot/js/pagination.js
+++
@@ -1,9 +0,0 @@
-﻿function ChangePage(page) {
-    $("#hdn_PageNumber").val(page);
-    LoadData();
-}
-function ChangePageSize(size) {
-    $("#hdn_PageSize").val(size);
-    $("#hdn_PageNumber").val(1);
-    LoadData();
-}
